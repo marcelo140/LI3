33ad7ddeb4102d99f1eabe47c3eb3cc936f860b6
escolhido <celocaridade@gmail.com>
2016-02-20 03:13:54 +0000

    Sales checking fixed

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ obj = main.o datacheck.o
 gereVendas: $(obj)
 	$(CC) $(obj) -o gereVendas
 
+debug: $(obj)
+	$(CC) $(obj) -g -o gereVendas
+
 main.o: datacheck.h
 datacheck.o: datacheck.h
 
--- datacheck.c
+++ datacheck.c
@@ -92,17 +92,17 @@ static int checkSale (char *line) {
 		switch(i) {
 			case 0: lnOk = checkProduct(token);
 							break;
-			case 1: lnOk = (price = strtof(token, NULL) > 0 && price < 999.99);
+			case 1: lnOk = ((price = strtof(token, NULL)) >= 0 && price <= 999.99);
 							break;
-			case 2: lnOk = (quant = atoi(token) >= 1 && quant <= 200);
+			case 2: lnOk = ((quant = atoi(token)) >= 1 && quant <= 200);
 							break;
-			case 3: lnOk = strcmp(token, "P") || !strcmp(token, "N");
+			case 3: lnOk = !strcmp(token, "P") || !strcmp(token, "N");
 							break;
 			case 4: lnOk = checkClient(token);
 							break;
 			case 5: lnOk = (month = atoi(token) >= 1 && month < 12);
 							break;
-			case 6: lnOk = (filial = atoi(token) >= 1 && filial < 3);
+			case 6: lnOk = ((filial = atoi(token)) >= 1 && filial <= 3);
 							break;
 			default: lnOk = 0;
 		}
