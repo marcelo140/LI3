ab4ab39434dd4297cc262b0880ad523a808b6d45
escolhido <celocaridade@gmail.com>
2016-04-04 22:14:09 +0100

    Added billing module

---
+++ src/fatglobal.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+#include "catalog.h"
+#include "fatglobal.h"
+
+struct fatoracao {
+	CATALOG l;
+};
+
+struct fatProduct {
+	double billed[MONTHS][BRANCHES][NP];
+};
+
+FATGLOBAL initFat() {
+		FATGLOBAL fat = malloc(sizeof (*fat));
+		fat->l = initCatalog();
+
+		return fat;
+}
---
+++ src/fatglobal.h
@@ -0,0 +1,15 @@
+#ifndef __FATGLOBAL__
+#define __FATGLOBAL__
+
+#include "generic.h"
+
+#define N 0
+#define P 1
+
+typedef struct fatoracao *FATGLOBAL;
+
+FATGLOBAL initFat(void);
+
+
+
+#endif
