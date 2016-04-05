ef799fc8630c85ec7066180412cdb3f54f584ffc
9thScientist <bc.tudo@gmail.com>
2016-04-05 22:22:38 +0000

    Added addFat() to fatglobal.c

--- src/fatglobal.c
+++ src/fatglobal.c
@@ -7,10 +7,14 @@ struct faturacao {
 	CATALOG l;
 };
 
-struct fatProduct {
+typedef struct fatProduct {
 	double billed[MONTHS][BRANCHES][NP];
-};
+} *FATPRODUCT;
 
+/**
+ * Inicia uma Faturação Global
+ * @return Nova Faturação Global
+ */
 FATGLOBAL initFat() {
 		FATGLOBAL fat = malloc(sizeof (*fat));
 		fat->l = initCatalog();
@@ -18,6 +22,29 @@ FATGLOBAL initFat() {
 		return fat;
 }
 
+/**
+ * Verifica se uma Faturação Global está vazia
+ * @param f Faturação Global a verificar
+ * @return true caso esteja vazia, false caso contrário
+ */
 bool isEmptyFat(FATGLOBAL f) {
 	return (f==NULL);
+}
+
+/**
+ * Adiciona uma venda à Faturação Global
+ * @param fat Faturação Global
+ * @param s SALE que reprensta a venda
+ * @return Faturação Global com a nova venda
+ */
+FATGLOBAL addFat(FATGLOBAL fat, SALE s) {
+	
+	char *p = fromProduct(getProduct(s));
+	int month = getMonth(s), branch = getBranch(s), mode = getMode(s);
+	double price = getPrice(s);
+	FATPRODUCT cntt = getCatalogContent(fat->l, p);
+
+	cntt->billed[month][branch][mode] += price;
+
+	return fat;
 }
