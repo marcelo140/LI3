cc935d37ddb2a0b95aaad8817d4235be71b8d942
escolhido <celocaridade@gmail.com>
2016-04-05 22:29:41 +0100

    Removed checkSaleLn

--- src/datacheck.c
+++ src/datacheck.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 
 #include "datacheck.h"
+#include "sales.h"
 
 #define BUFF_SIZE 35
-
-static int checkSaleLn(char *line, PRODUCTCAT product, CLIENTCAT client);
 
 CLIENTCAT writeCCat(FILE *file, CLIENTCAT cat, int *num) {
 	int n;
@@ -48,16 +47,17 @@ PRODUCTCAT writePCat(FILE *file, PRODUCTCAT cat, int *num) {
 }
 
 int checkSales (FILE *file, PRODUCTCAT products, CLIENTCAT clients, int *sucLn, int *failLn) {
-	int checked_line, suc, fail;
+	SALE s;
 	char buf[BUFF_SIZE], *line;
+	int suc, fail;
 
 	suc = fail = 0;
 
 	while(fgets(buf, BUFF_SIZE, file)) {
 		line = strtok (buf, "\n\r");
-		checked_line = checkSaleLn(line, products, clients);
+		s = readSale(line);
 
-		(checked_line) ? suc++ : fail++;
+		(isSale(s, products, clients)) ? suc++ : fail++;
 	}
 
 	*sucLn = suc;
@@ -65,39 +65,6 @@ int checkSales (FILE *file, PRODUCTCAT products, CLIENTCAT clients, int *sucLn, 
 
 	return 0;
 }
-
-static int checkSaleLn (char *line, PRODUCTCAT productCat, CLIENTCAT clientCat) {
-	int i, lnOk, quant, month, branch;
-	double price;
-	char *token;
-
-	lnOk = 1;
-	token = strtok(line, " ");
-
-	for (i = 0; lnOk && token != NULL; i++){
-		switch(i) {
-			case 0: lnOk = lookUpProduct(productCat, toProduct(token));
-						break;
-			case 1: lnOk = ((price = atof(token)) >= 0 && price <= 999.99);
-						break;
-			case 2: lnOk = ((quant = atoi(token)) >= 1 && quant <= 200);
-						break;
-			case 3: lnOk = !strcmp(token, "P") || !strcmp(token, "N");
-						break;
-			case 4: lnOk = lookUpClient(clientCat, toClient(token));
-						break;
-			case 5: lnOk = (month = atoi(token) >= 1 && month <= 12);
-						break;
-			case 6: lnOk = ((branch = atoi(token)) >= 1 && branch <= 3);
-						break;
-			default: lnOk = 0;
-		}
-		token = strtok(NULL, " ");
-	}
-
-	return lnOk;
-}
-
 
 void testsValidSales() {
 
