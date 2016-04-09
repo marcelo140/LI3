c531ae696ff93ef85edf5277802d8f6762852417
9thScientist <bc.tudo@gmail.com>
2016-04-09 15:48:33 +0000

    Added getters for vending

--- src/vending.c
+++ src/vending.c
@@ -2,18 +2,18 @@
 #include "vending.h"
 
 #define MONTHS 12
-#define NP 2
+#define PROMO 2
 
 struct vending{
-	double billed[MONTHS][NP];
-	int quantity[MONTHS][NP];
+	double billed[MONTHS][PROMO];
+	int quantity[MONTHS][PROMO];
 };
 
 /**
  * Inicializa a VENDING e coloca todos os seus valores a 0;
  * @return nova VENDING
  */
-VENDING initTransaction () {
+VENDING initVending() {
 	VENDING new = malloc (sizeof(*new));
 	int i;
 
@@ -29,37 +29,58 @@ VENDING initTransaction () {
 
 /**
  * Adicionar ao total faturado o valor faturado num dado mes num dado modo.
- * @param t VENDING a adicionar
+ * @param v VENDING a adicionar
  * @param m Mês da venda
  * @param MODE MODE_N caso seja normal, MODE_P caso tenha promoção
  * @param value Valor a somar
  * @return VENDING atualizada
  */
-VENDING addBilled(VENDING t, int month, int MODE, int value) {
-	t->billed[month][MODE] += value;
+VENDING addBilled(VENDING v, int month, int MODE, int value) {
+	v->billed[month][MODE] += value;
 
-	return t;	
+	return v;	
 }
 
 /**
  * Adicionar à quantidade total o valor num dado mes num dado modo.
- * @param t VENDING a adicionar
+ * @param v VENDING a adicionar
  * @param m Mês da venda
  * @param MODE MODE_N caso seja normal, MODE_P caso tenha promoção
  * @param value Valor a somar
  * @return VENDING atualizada
  */
+VENDING addQuantity(VENDING v, int month, int MODE, int value) {
+	v->quantity[month][MODE] += value;
 
-VENDING addQuantity(VENDING t, int month, int MODE, int value) {
-	t->quantity[month][MODE] += value;
+	return v;	
+}
 
-	return t;	
+/**
+ * Devolve o total faturado num dado mês numa dada promoção
+ * @param v VENDING
+ * @param month Mês em questão
+ * @param MODE Modo da promoção
+ * @return O total faturado
+ */
+double getBilled(VENDING v, int month, int MODE) {
+	return v->billed[month][MODE]; 
+}
+
+/**
+ * Devolve a quantidade total vendida num dado mês num dada promoção
+ * @param v VENDING
+ * @param month Mês em questão
+ * @param MODE Modo da promoção
+ * @return A quantidade total vendida
+ */
+int getQuantity(VENDING v, int month, int MODE) {
+	return v->quantity[month][MODE];
 }
 
 /**
  * Liberta o espaço ocupado pela VENDING t
  * @param t VENDING a libertar
  */
-void freeTransaction(VENDING t) {
-	free(t);
+void freeTransaction(VENDING v) {
+	free(v);
 }
