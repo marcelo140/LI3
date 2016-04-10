2ed45458babd450139441fc993fa9bf4acf8e42e
escolhido <celocaridade@gmail.com>
2016-04-10 23:36:57 +0100

    Added isEmpty revenue and getters

--- src/sales.h
+++ src/sales.h
@@ -5,15 +5,13 @@
 #include "clients.h"
 #include "products.h"
 
-#define SALE_SIZE 7
-
 #define MODE_N 0
 #define MODE_P 1
 
 #define M_CONTENT 0
 #define M_TOTAL 1
 
-typedef struct sale *SALE;
+typedef struct sale    *SALE;
 typedef struct revenue *REVENUE;
 
 SALE    initSale    ();
@@ -26,17 +24,24 @@ REVENUE updateRevenue (REVENUE r, int month, int branch, int MODE, double billed
 REVENUE addBilled     (REVENUE r, int month, int branch, int MODE, double value);
 REVENUE addQuantity   (REVENUE r, int month, int branch, int MODE, int value);
 
-bool isSale   (SALE sale, PRODUCTCAT prodCat, CLIENTCAT clientCat);
+bool isEmptyRev (REVENUE r);
+bool isSale     (SALE sale, PRODUCTCAT prodCat, CLIENTCAT clientCat);
 
-PRODUCT getProduct  (SALE s);
-CLIENT  getClient   (SALE s);
-double  getPrice    (SALE s);
-int     getQuant    (SALE s);
-int     getMonth    (SALE s);
-int     getBranch   (SALE s);
-int     getMode     (SALE s);
-double  getBilled   (REVENUE r, int month, int branch, int MODE);
-int     getQuantity (REVENUE r, int month, int branch, int MODE);
+PRODUCT getProduct (SALE s);
+CLIENT  getClient  (SALE s);
+double  getPrice   (SALE s);
+int     getQuant   (SALE s);
+int     getMonth   (SALE s);
+int     getBranch  (SALE s);
+int     getMode    (SALE s);
+
+double  getMonthBilled  (REVENUE r, int month,  double *normal, double *promo);
+double  getBranchBilled (REVENUE r, int branch, double *normal, double *promo);
+double  getBilled       (REVENUE r, int month,  int branch,  int MODE);
+
+int     getBranchQuant  (REVENUE r, int branch, int *normal, int *promo);
+int     getMonthQuant   (REVENUE r, int month,  int *normal, int *promo);
+int     getQuantity     (REVENUE r, int month, int branch, int MODE);
 
 void freeSale    (SALE s, int mode);
 void freeRevenue (REVENUE r);
