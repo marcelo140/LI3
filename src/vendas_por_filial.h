0010031f946d022ea57f6e55c20ea4a3ef237f62
9thScientist <bc.tudo@gmail.com>
2016-04-13 04:05:18 +0000

    advances in vendas_por_filial

--- src/vendas_por_filial.h
+++ src/vendas_por_filial.h
@@ -1,4 +1,14 @@
 #ifndef __VENDAS_FILIAL__
 #define __VENDAS_FILIAL__
 
+#include "sales.h"
+#include "generic.h"
+
+typedef struct filial *BRANCHSALES;
+
+BRANCHSALES initBranchSales ();
+BRANCHSALES addSaleToBranch (BRANCHSALES bs, SALE s);
+
+void freeBranchSale(BRANCHSALES bs);
+
 #endif
