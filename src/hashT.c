94ccf434c108dab1b05fe436c7f29fca57f7af42
9thScientist <bc.tudo@gmail.com>
2016-04-15 22:43:40 +0000

    Hash Table retry

--- src/hashT.c
+++ src/hashT.c
@@ -1,139 +1,93 @@
-/*********************************
- * 	HASH TABLE AUXILIAR À HEAP   *
- *********************************/
-
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include "hashT.h"
 
-#define MAX_STR_SIZE 10
-#define BASE_CAPACITY 31
-#define CONTENT(i) ht->cntt[i]
-#define EMPTY 0
-#define REMOVED -1
-#define USED 1
+#define BASE_CAPACITY 64
+#define EMPTY   0
+#define BUSY    1
+#define REMOVED 2
+
+#define HASH_CRAWLER(i) i < ht->capacity && ht->table[i].status != EMPTY && strcmp(ht->table[i].key, key) != 0
+#define CAPACITY        ht->capacity
+#define KEY(i)     		ht->table[i].key
+#define CONTENT(i) 		ht->table[i].content
+#define STATUS(i)  		ht->table[i].status
 
 struct hashCntt {
-	int position;
-	char status;
-	char seed[MAX_STR_SIZE];
+	char *key;
+	void *content;
+	int status;
 };
 
-struct table {
-	int	capacity;	
-	struct hashCntt* cntt;
+struct hasht {
+	struct hashCntt* table;
+	int capacity;
+	ht_init_t init;
+	ht_add_t add;
+	ht_free_t free;
 };
 
-static int hash(char *seed, int size);
-static HASHTABLE resizeHashT(HASHTABLE ht);
+static int Hash(char *key);
+static HASHT resizeHashT(HASHT ht);
 
-/**
- * Inicia uma HASHTABLE nova
- * @return Nova HASHTABLE
- */
-HASHTABLE initHashTable() {
-	HASHTABLE new = malloc(sizeof (*new));
+HASHT initHashT(ht_init_t init, ht_add_t add, ht_free_t free) {
+	HASHT new = malloc(sizeof(*new));
 
+	new->table = calloc(BASE_CAPACITY, sizeof(struct hashCntt));
 	new->capacity = BASE_CAPACITY;
-	new->cntt = calloc(BASE_CAPACITY, sizeof(struct hashCntt));
+	new->init = init;
+	new->add  = add;
+	new->free = free;
 
 	return new;
 }
 
-/**
- * Insere uma SALE e um apontador para o pai na HASHTABLE
- * Se já existe, apenas adiciona à REVENUE existente
- * @param ht HASHTABLE a atualizar
- * @param seed Hash a inserir
- * @param pos Respetiva posição
- * @return HASHTABLE atualizada
- */
-HASHTABLE insertHashT(HASHTABLE ht, char *seed, int pos) {
-	int p, n, i=1, cap = ht->capacity, loop=0;
-
-	p = hash(seed, cap);	
-	if (ht->cntt[p].status == EMPTY || ht->cntt[p].status == REMOVED) {
-		ht->cntt[p].position = pos;
-		strcpy(ht->cntt[p].seed, seed);
-		ht->cntt[p].status = USED;
-	}
-	else {
-		n = p;
-		for(n=p; !loop && CONTENT(n).status == USED ; i++) {
-			n = (p + i*i) % cap;
-			if (n == p) loop = 1;	
-			i++;	
-		}
-
-		if (loop) {
-			ht = resizeHashT(ht);
-			ht = insertHashT(ht, seed, pos);
-		}
-	}
+HASHT insertHashT(HASHT ht, char* key, void* content) {
+	int i, hash, p;
 	
+	p = hash = Hash(key) & (CAPACITY - 1);
+
+	for (i=0; HASH_CRAWLER(p); i++)
+		p = hash + (i*i) & (CAPACITY - 1);
+
+	if (i == CAPACITY) {
+		ht = resizeHashT(ht);
+		ht = insertHashT(ht, key, content);
+	} else {
+		if (STATUS(p) != BUSY) {
+			STATUS(p) = BUSY;
+			strcpy(KEY(p), key);
+			CONTENT(p) = ht->init();
+		}
+		CONTENT(p) = ht->add(CONTENT(p), content);
+	}
+
 	return ht;
 }
 
-int getPosition(HASHTABLE ht, char *seed) {
-	int h, p, i;
+static int Hash(char *key) {
+	int i, hash = 5381;
+	char c;
 
-	h = hash(seed, ht->capacity);
-	
-	for(p=h, i=0; strcmp(CONTENT(p).seed, seed) != 0 && i < ht->capacity; i++)
-		p = (h + (i*i)) % ht->capacity;
-	
-	if (i >= ht->capacity) p = -1;
-	
-	return p;
+	for(i=0; (c = key[i]) ; i++)
+		hash = (hash << 5) + hash + c;
+
+	return hash;
 }
 
-/** 
- * Liberta o espaço ocupado por uma HashTable
- * @param ht HASHTABLE a libertar
- */
-void freeHashTable(HASHTABLE ht) {
-	free(ht->cntt);
-	free(ht);
+static HASHT resizeHashT(HASHT ht){
+	HASHT new = malloc(sizeof(*new));
+	int i;
+
+	new->capacity = ht->capacity *2;
+	new->init     = ht->init;
+	new->free     = ht->free;
+	new->add 	  = ht->add;
+
+	for(i=0; i < ht->capacity; i++)
+		if (STATUS(i) == BUSY) 
+			insertHashT(new, KEY(i), CONTENT(i));
+
+	freeHashT(ht);
+	return new;
 }
-
-/**
- * Cria uma HASH de uma string para uma dada HASHTABLE
- * @param seed String que serve como seed
- * @param size Tamanho da tabela de Hash
- */
-static int hash(char *seed, int size) {
-	int i, r=0;
-
-	/* Este algoritmo de criação de hashes deve ser melhorado */
-	for (i=0; seed[i] != '\0' && i < MAX_STR_SIZE; i++) {
-		r += seed[i];
-	} 
-
-	r %= size;
-
-	return r;
-}
-
-static HASHTABLE resizeHashT(HASHTABLE ht) {
-	int i, j, newCap;
-	struct hashCntt* newCntt;
-
-	newCap = ht->capacity * 2;
-	newCntt = calloc(newCap, sizeof(struct hashCntt));
-
-	for (i=0; i < ht->capacity; i++){
-		if (CONTENT(i).status == USED){
-			j = hash(CONTENT(i).seed, newCap);
-			newCntt[j].position = CONTENT(i).position;
-			strcpy(newCntt[j].seed, CONTENT(i).seed);
-			newCntt[j].status = USED;
-		} 
-	}
-
-	free(ht->cntt);
-	ht->cntt = newCntt;
-	
-	return ht;
-}
-
-
