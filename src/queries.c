2d2dc2e559ae9d0d4d1b262cf46a88bc28a603fd
9thScientist <bc.tudo@gmail.com>
2016-04-16 22:05:45 +0000

    Queries added

---
+++ src/queries.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "queries.h"
+
+#define MAX_SIZE 128
+
+PRINTSET query5(BRANCHSALES bs, CLIENT client) {
+	PRINTSET print = initPrintSet(12);
+	char str[MAX_SIZE];
+	int i, *quantity;
+
+	quantity = getClientQuant(bs, client);
+
+	for(i=0; i < 12; i++){
+		sprintf(str, "Mês %d:\t%d", i+1, quantity[i]);
+		print = addToPrintSet(print, str);
+	}
+
+	free(quantity);
+	return print;
+}
---
+++ src/queries.h
@@ -0,0 +1,9 @@
+#ifndef __QUERIES_H__
+#define __QUERIES_H__
+
+#include "interpreter.h"
+#include "vendas_por_filial.h"
+
+PRINTSET query5(BRANCHSALES bs, CLIENT client);
+
+#endif
