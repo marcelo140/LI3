34d3a88681484415b066e53673251c6f39cfa6c7
escolhido <celocaridade@gmail.com>
2016-04-19 22:51:16 +0100

    Minor change

--- src/fatglobal.c
+++ src/fatglobal.c
@@ -66,7 +66,7 @@ FATGLOBAL fillFat (FATGLOBAL fat, PRODUCTCAT p) {
 }
 
 FATGLOBAL addFat(FATGLOBAL fat, SALE s) {
-	REVENUE r;
+	REVENUE rev;
 	char prod[PRODUCT_LENGTH];
 
 	fromProduct(getProduct(s), prod);
